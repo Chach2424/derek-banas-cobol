0d6da76fb9cc40da85056663f090e0976dd57040
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-01 17:04:25 -0500

    Add comparison operators and more conditional statements to coboltut3.cob

--- coboltut3.cob
+++ coboltut3.cob
@@ -4,9 +4,11 @@ PROGRAM-ID. coboltut3.
 AUTHOR. Tajul Tijerina
 DATE-WRITTEN.November 1st 2022
 ENVIRONMENT DIVISION.
+
 CONFIGURATION SECTION.
 SPECIAL-NAMES.
        CLASS PassingScore IS "A" THRU "C", "D".
+
 DATA DIVISION.
 FILE SECTION.
 WORKING-STORAGE SECTION.
@@ -22,6 +24,7 @@ WORKING-STORAGE SECTION.
        88 IsEven     VALUE "2", "4", "6", "8".
        88 LessThan5  VALUE "1" THRU "4".
        88 ANumber    VALUE "0" THRU "9".          
+
 PROCEDURE DIVISION.
 DISPLAY "Enter your age: " WITH NO ADVANCING
 ACCEPT Age
@@ -29,5 +32,28 @@ IF Age > 18 THEN
        DISPLAY "You can vote"
 ELSE 
        DISPLAY "You can't vote"
-END-IF               
+END-IF   
+
+*> > GREATER THAN
+*> NOT > NOT GREATER THAN
+*> < LESS THAN
+*> NOT < NOT LESS THAN
+*> = EQUAL TO
+*> NOT = NOT EQUAL TO
+
+IF Age LESS THAN 5 THEN
+       DISPLAY "Stay home"
+END-IF
+IF Age = 5 THEN
+       DISPLAY "Go to kindergarten"
+END-IF
+IF Age > 5 AND Age < 18 THEN
+       COMPUTE Grade = Age - 5
+       DISPLAY "Go to grade " Grade
+END-IF        
+IF Age GREATER THAN OR EQUAL TO 18 THEN
+       DISPLAY "Go to college"
+END-IF
+
+
 STOP RUN.
