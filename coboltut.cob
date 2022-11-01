9bb5fceb6acb96136a7f5bb4c0823d8827f1b6aa
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-01 11:23:29 -0500

    Add more code to coboltut.cob

--- coboltut.cob
+++ coboltut.cob
@@ -16,6 +16,28 @@ WORKING-STORAGE SECTION.
        02 SSArea    PIC 999.
        02 SSGroup   PIC 99.
        02 SSSerial  PIC 9999.
+01 PIValue CONSTANT AS 3.14.
+*> This is a comment
+*> Figurative Constants
+*> ZERO/ZEROES       
+*> SPACE/SPACES
+*> HIGH-VALUE/S
+*> LOW-VALUE/S
 
 PROCEDURE DIVISION.
+DISPLAY "What is your name? " WITH NO ADVANCING
+*> NO ADVANCING means you won't have a new line when it prints to screen
+ACCEPT UserName
+DISPLAY "Hello, " UserName
+MOVE ZERO TO UserName
+DISPLAY UserName
+DISPLAY "Enter 2 values to sum "
+ACCEPT Num1
+ACCEPT Num2
+COMPUTE Total = Num1 + Num2
+DISPLAY Num1 " + " Num2 " = " Total
+DISPLAY "Enter your social security number "
+ACCEPT SSNum
+DISPLAY "Area " SSArea
 
+STOP RUN.
