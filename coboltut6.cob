5a7fcf80779a773c8f2943c47342019cfd877dd7
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-02 15:06:18 -0500

    Add ForLoop to coboltut6.cob

--- coboltut6.cob
+++ coboltut6.cob
@@ -8,10 +8,20 @@ CONFIGURATION SECTION.
 DATA DIVISION.
 FILE SECTION.
 WORKING-STORAGE SECTION.
-       
+01 Ind PIC 9(1) VALUE 0.       
 PROCEDURE DIVISION.
+PERFORM OutputData WITH TEST AFTER UNTIL Ind > 5
+       GO TO ForLoop.
+
+OutputData.
+       DISPLAY Ind.
+       ADD 1 TO Ind.
+
+ForLoop.
+       PERFORM OutputData2 VARYING Ind FROM 1 BY 1 UNTIL Ind = 5
+       STOP RUN.
+
+OutputData2.
+       DISPLAY Ind.
 
 
-
-
-STOP RUN.
