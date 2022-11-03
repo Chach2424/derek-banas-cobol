e49f705b9f55f7cbd6842c87733f73fb28b0543e
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-03 16:45:02 -0500

    Add Table1 to WORKING-STORAGE SECTION and code in PROCEDURE DIVISION to populate table

--- coboltut12.cob
+++ coboltut12.cob
@@ -5,8 +5,16 @@ PROGRAM-ID. coboltut12.
 DATA DIVISION.
 
 WORKING-STORAGE SECTION.
+01 Table1.
+       02 Friend PIC X(15) OCCURS 4 TIMES.
 
 PROCEDURE DIVISION.
+MOVE 'Joy' TO Friend(1).
+MOVE 'Willow' TO Friend(2).
+MOVE 'Ivy' TO Friend(3).
+
+DISPLAY Friend(1).
+DISPLAY Table1.
 
 STOP RUN.          
 
