56e1f45608e267b5fb3895c48869d088247c9390
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-03 10:55:52 -0500

    Add code to PROCEDURE DIVISION in coboltut10.cob to write data to Customer.dat

--- coboltut10.cob
+++ coboltut10.cob
@@ -3,16 +3,38 @@ IDENTIFICATION DIVISION.
 PROGRAM-ID. coboltut9.
 AUTHOR. 
 DATE-WRITTEN.November 2nd 2022
+
 ENVIRONMENT DIVISION.
-CONFIGURATION SECTION.
+INPUT-OUTPUT SECTION.
+FILE-CONTROL.
+       SELECT CustomerFile ASSIGN TO "Customer.dat"
+           ORGANIZATION IS LINE SEQUENTIAL
+           ACCESS IS SEQUENTIAL.
+
 DATA DIVISION.
 FILE SECTION.
+FD CustomerFile.
+01 CustomerData.
+       02 IDNum PIC 9(5).
+       02 CustName.
+           03 FirstName PIC X(15).
+           03 LastName PIC X(15). 
 
 WORKING-STORAGE SECTION.
-
+01 WSCustomer.
+       02 WSIDNum PIC 9(5).
+       02 WSCustName.
+           03 WSFirstName PIC X(15).
+           03 WSLastName PIC X(15). 
 
 PROCEDURE DIVISION.
-
+OPEN OUTPUT CustomerFile.
+       MOVE 00001 TO IDNum.
+       MOVE 'Doug' TO FirstName.
+       MOVE 'Thomas' TO LastName.
+       WRITE CustomerData
+       END-WRITE.
+CLOSE CustomerFile.
 
 STOP RUN.
 
