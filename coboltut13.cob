66cb95a3bcace3ad26b317701c424f7864143f1e
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-04 10:59:07 -0500

    Add code in WORKING-STORAGE SECTION to add dollar signs to a float

--- coboltut13.cob
+++ coboltut13.cob
@@ -20,13 +20,24 @@ WORKING-STORAGE SECTION.
 01 ChangeMe.
        02 TextNum PIC X(6).
        02 FloatNum REDEFINES TextNum PIC 9(4)V99.
+01 StrNum PIC X(7).
+01 SplitNum.
+       02 WNum PIC 9(4) VALUE ZERO.
+       02 FNum PIC 99 VALUE ZERO.
+01 FlNum REDEFINES SplitNum PIC 9999V99.
+01 DollarNum PIC $$,$$9.99.
 
 PROCEDURE DIVISION.
 DISPLAY Shirt(1).
 MOVE '123456' TO TextNum.
-DISPLAY FloatNum
-
-
+DISPLAY FloatNum.
+DISPLAY "Enter a float: " WITH NO ADVANCING
+ACCEPT StrNum
+UNSTRING StrNum
+       DELIMITED BY "." OR SPACES
+       INTO WNum, FNum
+MOVE FlNum TO DollarNum
+DISPLAY DollarNum.
 
 STOP RUN.          
 
