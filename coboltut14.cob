48dab2b3efb9094f6650b3733612fd69dc5a2ecc
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-04 11:46:19 -0500

    Modify code in PROCEDURE DIVISION to MERGE data in files, and student4.dat is the output

--- coboltut14.cob
+++ coboltut14.cob
@@ -5,35 +5,40 @@ ENVIRONMENT DIVISION.
 INPUT-OUTPUT SECTION.
 FILE-CONTROL.
        SELECT WorkFile ASSIGN TO 'work.tmp'.
-       SELECT OrgFile ASSIGN TO 'student.dat'
+       SELECT File1 ASSIGN TO 'student.dat'
            ORGANIZATION IS LINE SEQUENTIAL.
-       SELECT SortedFile ASSIGN TO 'student2.dat'
+       SELECT File2 ASSIGN TO 'student3.dat'
+           ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT NewFile ASSIGN TO 'student4.dat'
            ORGANIZATION IS LINE SEQUENTIAL.
 
 DATA DIVISION.
 FILE SECTION.
-FD OrgFile.
+FD File1.
 01 StudData.
        02 IDNum PIC 9.
        02 StudName PIC X(10).
-
+FD File2.
+01 StudData2.
+       02 IDNum2 PIC 9.
+       02 StudName2 PIC X(10).
 *> SD is going to be used whenever you're working with sorted files
 SD WorkFile.
 01 WStudData.
        02 WIDNum PIC 9.
        02 WStudName PIC X(10).
-FD SortedFile.
-01 SStudData.
-       02 SIDNum PIC 9.
-       02 SStudName PIC X(10).
+FD NewFile.
+01 NStudData.
+       02 NIDNum PIC 9.
+       02 NStudName PIC X(10).
 
 WORKING-STORAGE SECTION.
 
 
 PROCEDURE DIVISION.
-SORT WorkFile ON ASCENDING KEY SIDNum
-       USING OrgFile
-       GIVING SortedFile
+MERGE WorkFile ON ASCENDING KEY NIDNum
+       USING File1, File2
+       GIVING NewFile
 
 STOP RUN.          
 
