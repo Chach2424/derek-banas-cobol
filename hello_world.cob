1217f7a7ec42b550fc80ba3f6a48eae30e32acee
Tajul Tijerina <tajultijerina@protonmail.com>
2022-11-07 12:01:12 -0600

    Add an ironic hello_world.cob as final commit

---
+++ hello_world.cob
@@ -0,0 +1,22 @@
+      *****************************************************************
+      * Program name:   hello_world                               
+      * Original author: MYNAME                                
+      *
+      * Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 MYNAME  Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello_world.
+       AUTHOR. MYNAME. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 11/07/2022. 
+       DATE-COMPILED. 11/07/2022. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+       PROCEDURE DIVISION.
+       DISPLAY "Hello, world."
+       STOP RUN.
+       
